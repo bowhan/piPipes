4e93140242016733b5097b048223d75386c9a20c
bowhan <bo.han@umassmed.edu>
2014-03-15 18:47:38 -0400

    update README... add GPLv3 header ... fix typo in the README

--- bin/piper_RNASeq.sh
+++ bin/piper_RNASeq.sh
@@ -1,15 +1,21 @@
 
-# RNASeq pipeline single library mode
-# piper 
-# https://github.com/bowhan/piper.git
-# An integrated pipeline for piRNA and transposon analysis 
-# from small RNA Seq, RNASeq, CAGE/Degradome/RACE, ChIP-Seq and Genomic-Seq
-# Wei Wang (wei.wang2@umassmed.edu)
-# Bo W Han (bo.han@umassmed.edu, bowhan@me.com)
-# the Zamore lab and the Weng lab
-# Howard Hughes Medical Institute
-# RNA Therapeutics Institute
-# University of Massachusetts Medical School
+# piper, a pipeline collection for PIWI-interacting RNA (piRNA) and transposon analysis
+# Copyright (C) <2014>  <Bo Han, Wei Wang, Phillip Zamore, Zhiping Weng>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 
 ##########
 # Config #
